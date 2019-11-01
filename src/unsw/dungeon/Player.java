e8f7487beda005ad0d207af89435dd0379971490
Aussie-Panda <1341572805@qq.com>
2019-11-01 22:12:04 +1100

    update player add sword class

--- src/unsw/dungeon/Player.java
+++ src/unsw/dungeon/Player.java
@@ -1,5 +1,7 @@
 package unsw.dungeon;
 
+import java.util.ArrayList;
+
 /**
  * The player entity
  * @author Robert Clifton-Everest
@@ -8,7 +10,11 @@ package unsw.dungeon;
 public class Player extends Entity implements Movable {
 
     private Dungeon dungeon;
-
+    private int treasure;
+    
+    // a back pack contains multiple collectable items
+    private ArrayList<Collectable> backPack;
+    
     /**
      * Create a player positioned in square (x,y)
      * @param x
@@ -17,9 +23,64 @@ public class Player extends Entity implements Movable {
     public Player(Dungeon dungeon, int x, int y) {
         super(x, y);
         this.dungeon = dungeon;
+        this.backPack = new ArrayList<Collectable>();
+        this.treasure = 0;
     }
 
-    @Override
+    // pick up all the collectable items
+    public void pickUp (Collectable c) {
+    	
+    	if (c.getClass() == null ) {
+    		System.out.println("Can't pick up not a collectable object");
+    	} 
+    	
+    	if (addToBag (c) == true) return;
+    	
+    	else if (c.getClass().isInstance(Tressure.class)) {
+    		this.treasure++;
+    	}
+    }
+    
+
+    /**
+     * A helper to the pickUp function use to check if 
+     * exits a collectable in the bag.
+     * @param c
+     * @return if can successfully add to bag return true
+     * else false
+     */
+    public boolean addToBag (Collectable c) {
+    	int flag = 0;
+    	if (c.getClass().isInstance(Key.class)) {
+    		c = (Key) c;
+    		flag = 1;
+    	}
+    	
+    	if (c.getClass().isInstance(Sword.class)) {
+    		c = (Sword) c;
+    		flag = 1;
+    	}
+    	
+    	// if it can be pick up to bag and bag doesn't 
+    	// contains the item, add to bag retrun success
+    	if (!backPack.contains(c) && (flag == 1)) {
+			backPack.add(c);
+			return true;
+		}
+    	return false;
+    }
+    
+    
+    
+    
+    public ArrayList<Collectable> getBackPack() {
+		return backPack;
+	}
+
+
+
+
+	@Override
     public void moveUp() {
         if (getY() > 0)
             y().set(getY() - 1);
