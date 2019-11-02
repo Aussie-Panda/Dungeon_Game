6453c777cbfbbad69f05157a378aab55cb9723ad
Aussie-Panda <1341572805@qq.com>
2019-11-02 22:20:58 +1100

    eed fix collectable

--- src/test/testCollectable.java
+++ src/test/testCollectable.java
@@ -12,8 +12,8 @@ import unsw.dungeon.Point;
 public class testCollectable {
 
 	@Test
-    void testPlayerPicpKeys(){
-		System.out.println("Testing Player move left -----");
+    void testPlayerPickKeys(){
+		System.out.println("Testing Player pick keys -----");
         Dungeon dungeon = new Dungeon(4, 4);
         Player p = new Player(dungeon, 0, 0);
         dungeon.addEntity(p);
@@ -30,8 +30,34 @@ public class testCollectable {
         assertEquals(p.getBackPack().contains(k), true);
         
         p.moveRight();
+        //fail becuz already has one key
         assertEquals(p.getBackPack().contains(k1), false);
-        
 
     }
+	
+	@Test
+	void testPlayerPicpKeysAfterConsumeKeys(){
+		System.out.println("Testing Player pick keys consume key pick"
+				+ "another key -----");
+        Dungeon dungeon = new Dungeon(4, 4);
+        Player p = new Player(dungeon, 0, 0);
+        dungeon.addEntity(p);
+        
+        Key k = new Key(dungeon, 0, 1, 0);
+        dungeon.addEntity(k);
+        Key k1 = new Key(dungeon, 1, 2, 0);
+        dungeon.addEntity(k1);
+        
+        p.moveRight();
+        
+
+        assertEquals(p.getBackPack().contains(k1), false);
+        assertEquals(p.getBackPack().contains(k), true);
+        p.consumeKey(0);
+        p.moveRight();
+
+        assertEquals(p.getBackPack().contains(k1), true);
+
+    }
+	
 }
