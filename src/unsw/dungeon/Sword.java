35fc8691d3d47c5d7823995e2b22805cecb0ea58
Aussie-Panda <1341572805@qq.com>
2019-11-02 14:17:06 +1100

    genralized interact

--- src/unsw/dungeon/Sword.java
+++ src/unsw/dungeon/Sword.java
@@ -16,10 +16,27 @@ public class Sword extends Entity implements Collectable {
     }
 	
 	@Override
-	public void interact(Player p) {
+	public void interact(Player p, String direction) {
 		// TODO Auto-generated method stub
 		collect(p);
-	}
+		if (direction == "up") {
+			p.getPt().setUp();
+		}
+		
+		if (direction == "down") {
+			p.getPt().setDown();
+		}
+		
+		if (direction == "left") {
+			p.getPt().setLeft();
+		}
+		
+		if (direction == "right") {
+			p.getPt().setRight();
+		}
+}
+	
+
 
 
 
--- src/unsw/dungeon/Tressure.java
+++ src/unsw/dungeon/Tressure.java
@@ -17,10 +17,26 @@ public class Tressure extends Entity implements Collectable {
     }
     
 	@Override
-	public void interact(Player p) {
+	public void interact(Player p, String direction) {
 		// TODO Auto-generated method stub
 		collect(p);
+		if (direction == "up") {
+			p.getPt().setUp();
+		}
+		
+		if (direction == "down") {
+			p.getPt().setDown();
+		}
+		
+		if (direction == "left") {
+			p.getPt().setLeft();
+		}
+		
+		if (direction == "right") {
+			p.getPt().setRight();
+		}
 	}
+	
     
 	@Override
 	public void collect(Player p) {
