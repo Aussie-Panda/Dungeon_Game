f210249655def76db095a6948f729bf913aa1248
KatrinaaDing <xiaoding1998@hotmail.com>
2019-11-03 15:14:56 +1100

    finish key test

--- src/test/TestDoorAndKey.java
+++ src/test/TestDoorAndKey.java
@@ -5,6 +5,7 @@ import unsw.dungeon.*;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class TestDoorAndKey {
@@ -55,5 +56,62 @@ class TestDoorAndKey {
         // player pass door again without key
         p.moveDown();
         assertEquals(p.getPt(), new Point(1, 2));
+    }
+
+    @Test
+    void testDiffKey() {
+        Dungeon dungeon = new Dungeon(4, 4);
+        Player p = new Player(dungeon, 0, 0);
+        dungeon.addEntity(p);
+
+        dungeon.addEntity(new Door(dungeon, 0, 1,1));
+        dungeon.addEntity(new Door(dungeon, 1, 2, 1));
+        dungeon.addEntity(new Key(dungeon, 0, 1, 2 ));
+
+        // player picks k1 and goes to open d2
+        p.moveDown();
+        p.moveDown();
+        p.moveRight();
+        p.moveRight();
+        p.moveUp();
+
+        // door shouldn't be opened
+        assertEquals(p.getPt(), new Point(2,  2));
+
+        // player goes tto open d1
+        p.moveLeft();
+        p.moveUp();
+
+        // player should be able to goes through door
+        assertEquals(p.getPt(), new Point(1,0));
+    }
+
+    @Test
+    void testPickKey(){
+        Dungeon dungeon = new Dungeon(4, 4);
+        Player p = new Player(dungeon, 0, 0);
+        dungeon.addEntity(p);
+
+        Key k1 = new Key(dungeon, 0, 1, 1 );
+        dungeon.addEntity(k1);
+        Key k2 = new Key(dungeon, 1, 1, 2 );
+        dungeon.addEntity(k2);
+
+        // player pick k1
+        p.moveDown();
+        p.moveRight();
+
+        // k1 is picked
+        assertTrue(p.hasKey(0));
+        assertFalse(dungeon.getEntities().contains(k1));
+
+        // player tries to pick k2;
+        p.moveDown();
+        p.moveDown();
+
+        // key cannot be picked
+        assertFalse(p.hasKey(1));
+        assertTrue(dungeon.getEntities().contains(k2));
+
     }
 }
