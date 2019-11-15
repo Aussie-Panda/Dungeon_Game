15534d0cd7c50cd4e4c414ea83f5a77211d02cf0
KatrinaaDing <xiaoding1998@hotmail.com>
2019-11-15 23:07:16 +1100

    typo

--- src/unsw/dungeon/DungeonApplication.java
+++ src/unsw/dungeon/DungeonApplication.java
@@ -14,16 +14,16 @@ public class DungeonApplication extends Application {
     public void start(Stage primaryStage) throws IOException {
     	StartScreen startScreen = new StartScreen(primaryStage);
     	WinScreen winScreen = new WinScreen(primaryStage);
-    	//LoseScreen loseScreen = new LoseScreen(primaryStage);
+    	LoseScreen loseScreen = new LoseScreen(primaryStage);
     	DungeonScreen dungeonScreen = new DungeonScreen(primaryStage, "advanced.json");
 
     	startScreen.getController().setDungeonScreen(dungeonScreen);
     	dungeonScreen.getController().setStartScreen(startScreen);
     	dungeonScreen.getController().setWinScreen(winScreen);
-    	//dungeonScreen.getController().setLoseScreen(loseScreen);
+    	dungeonScreen.getController().setLoseScreen(loseScreen);
     	
     	winScreen.getController().setStartScreen(startScreen);
-    	//loseScreen.getController().setDungeonScreen(dungeonScreen);
+    	loseScreen.getController().setDungeonScreen(dungeonScreen);
     	
     	startScreen.start();
 
